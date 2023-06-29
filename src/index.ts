f01f5992d56265be71f78abe420246d113fa653c
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-06-30 00:02:05 +0800

    0.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export * from './badge.js'
+import { R3Badge } from './badge.js'
+
+export default R3Badge
--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,18 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [
-    dts({ insertTypesEntry: true })
-  ],
+  plugins: [dts({ insertTypesEntry: true })],
   build: {
     target: 'esnext',
     lib: {
       entry: 'src/index.ts',
-      fileName: 'index',
-      name: 'brand'
-    },
-    rollupOptions: {
-      output: {
-        exports: 'named'
-      }
+      name: 'fireworks-js',
+      formats: [
+        'es',
+        'cjs',
+        'umd'
+      ],
+      fileName: (format) => `index.${format}.js`
     }
   }
 })
